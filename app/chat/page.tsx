7e6ff01afcf2b88f53857fdc4c46b9de4d5ece29
Mustafa Celik <celikmus@gmail.com>
2024-09-15 03:43:17 +0100

    Simplified form submission

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { FormEvent } from 'react'
 import { ClientMessage } from './actions'
 import { useActions, useUIState } from 'ai/rsc'
 import { generateId } from 'ai'
@@ -9,21 +9,20 @@ import { generateId } from 'ai'
 export const maxDuration = 30
 
 export default function Home() {
-  const [input, setInput] = useState<string>('')
   const [conversation, setConversation] = useUIState()
   const { continueConversation } = useActions()
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
     setConversation((currentConversation: ClientMessage[]) => [
       ...currentConversation,
-      { id: generateId(), role: 'user', display: input },
+      { id: generateId(), role: 'user', display: event.target.message.value },
     ])
-    const message = await continueConversation(input)
+    const message = await continueConversation(event.target.message.value)
     setConversation((currentConversation: ClientMessage[]) => [
       ...currentConversation,
       message,
     ])
-    setInput('')
   }
 
   return (
@@ -36,27 +35,16 @@ export default function Home() {
             </div>
           ))}
         </div>
-        <div className="my-4 flex caret-black">
+        <form className="my-4 flex caret-black" onSubmit={handleSubmit}>
           <input
             type="text"
-            value={input}
+            name="message"
             className="min-w-[400px] rounded-xl px-2 text-gray-900"
-            onChange={(event) => {
-              setInput(event.target.value)
-            }}
-            onKeyDown={(event) => {
-              if (event.key === 'Enter') {
-                handleSubmit()
-              }
-            }}
           />
-          <button
-            className="ml-2 rounded-xl bg-green-800 p-2 text-gray-100"
-            onClick={handleSubmit}
-          >
+          <button className="ml-2 rounded-xl bg-green-800 p-2 text-gray-100">
             Send Message
           </button>
-        </div>
+        </form>
       </div>
     </div>
   )
